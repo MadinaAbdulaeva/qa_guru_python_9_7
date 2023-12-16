544a5f7adcd077026b40579a4477a8787bf62415
Madina Abdulaeva <abdulaeva.mad@yandex.ru>
2023-12-16 15:02:11 +0300

    Adding libraries to requirements.txt, Updating functions

--- test_read_archive.py
+++ test_read_archive.py
@@ -13,33 +13,32 @@ resources = os.path.join(CURRENT_DIR, "resources")
 zip_path = os.path.join(resources, "test_archive.zip")
 
 def test_pdf_file(create_directory, archive):
-    with ZipFile('resources/test_archive.zip') as archive:
-        file = archive.read('example.pdf')
-        file_stats = os.stat(file)
-        print(file_stats)
-        print(f'File size in bytes is {file_stats.st_size}.')
-        reader = PdfReader('tmp/example.pdf')
-        assert file_stats.st_size == archive.getinfo('example.pdf').file_size == 38329
-        assert len(reader.pages) == 0
-        assert "Пример PDF файла" in reader.pages[0]
+    with ZipFile(os.path.join(resources, 'test_archive.zip')) as archive:
+        with archive.open('example.pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            assert len(reader.pages) == 1
+            assert "Пример PDF файла" in reader.pages[0].extract_text()
+            file_stats = os.stat(os.path.join(path, 'example.pdf'))
+            assert file_stats.st_size == archive.getinfo('example.pdf').file_size == 38329
+
 
 def test_xlsx_file(create_directory, archive):
-    with ZipFile('resources/test_archive.zip') as archive:
-        file = archive.read('import_company.xlsx')
-        workbook = load_workbook(file)
-        sheet = workbook.active
-        assert archive.getinfo('import_company.xlsx').file_size == 6809
-        assert len(workbook.sheetnames) == 1
-        assert sheet.max_row == 10
-        assert sheet.max_column == 5
-        assert sheet.cell(row=4, column=3).value == 'Отдел сервисного обслуживания'
+    with ZipFile(os.path.join(resources, 'test_archive.zip')) as archive:
+        with archive.open('import_company.xlsx') as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
+            assert archive.getinfo('import_company.xlsx').file_size == 6809
+            assert len(workbook.sheetnames) == 1
+            assert sheet.max_row == 10
+            assert sheet.max_column == 5
+            assert sheet.cell(row=4, column=3).value == 'Отдел сервисного обслуживания'
 
 def test_csv_file():
-    with ZipFile('resourses/test_archive.zip') as zip_file:
-        file = archive.read('gross-domestic-product-june-2023-quarter.csv')
-        reader = csv.DictReader(file)
-        print('Имена полей: ', reader.fieldnames)
-        assert archive.getinfo('gross-domestic-product-june-2023-quarter.csv').file_size == 19034868
+    with ZipFile(os.path.join(resources, 'test_archive.zip')) as archive:
+        with archive.open('gross-domestic-product-june-2023-quarter.csv') as csv_file:
+            reader = csv.DictReader(csv_file)
+            print('Имена полей: ', reader.fieldnames)
+            assert archive.getinfo('gross-domestic-product-june-2023-quarter.csv').file_size == 19034868
 
 
 
